37bd8f3af9571d3c3e3b8717d20b1a2ddadf9f3f
JPSS14 <jp_scarabelli@hotmail.com>
2021-03-29 18:23:03 -0300

    quiz-react UPDATE 38: Added prefix to PlayBox and TrainingBox

--- src/components/PlayBox.tsx
+++ src/components/PlayBox.tsx
@@ -2,12 +2,13 @@ import Link from "next/link";
 import styles from '../styles/PlayBox.module.css';
 
 export function PlayBox() {
+    const prefix = '/quiz-react';
     return (
         <div className={styles.playBox}>
             <div className={styles.playBoxContent}>
                 <Link href="/play-mode">
                     <a>
-                        <img src="captain-america-shield.png" />
+                        <img src={`${prefix}captain-america-shield.png`} />
                     Jogar
                 </a>
                 </Link>
--- src/components/TrainingBox.tsx
+++ src/components/TrainingBox.tsx
@@ -2,12 +2,13 @@ import Link from 'next/link';
 import styles from '../styles/TrainingBox.module.css';
 
 export function TrainingBox() {
+    const prefix = '/quiz-react';
     return (
         <div className={styles.trainingBox}>
             <div className={styles.trainingBoxContent}>
                 <Link href="/training-mode">
                     <a>
-                        <img src="shield.webp" />
+                        <img src={`${prefix}/shield.webp`} />
                         Treinar
                     </a>
                 </Link>
